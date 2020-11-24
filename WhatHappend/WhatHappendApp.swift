cf83b3aeeadf49774066449103e46c36f686b848
luwenxull <992273307@qq.com>
2020-11-24 22:43:18 +0800

    Initial Commit

---
+++ WhatHappend/WhatHappendApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatHappendApp.swift
+//  WhatHappend
+//
+//  Created by 陆雯旭 on 2020/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatHappendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
